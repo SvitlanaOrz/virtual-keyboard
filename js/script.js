f6cf2ed7d18b686e08e0b5e114e9194c6d6d465e
SvitlanaOrz <svitlana.orz@gmail.com>
2023-04-28 13:07:19 +0300

    fix: make the same values on the screen and keyboard

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 let buttons = document.querySelectorAll(".button");
+let spetialButtons = document.querySelectorAll(".special-button");
 let textButtons = document.querySelectorAll(".text-button");
 let keyboardScreen = document.querySelector("#keyboard-screen");
 let capsLock = document.querySelector(".CapsLock");
-
+let pressSpace = [];
 function showNumber(event) {
   keyboardScreen.innerHTML =
     keyboardScreen.innerHTML + event.srcElement.innerText;
@@ -37,25 +38,39 @@ function changeTextSize() {
 
 capsLock.addEventListener("click", changeTextSize);
 
-function showKeyboardNumber(event) {
-  keyboardScreen.innerHTML = keyboardScreen.innerHTML + event.key;
+function changeLanguage(event) {
   console.log(event.code);
+}
+
+function showKeyboardNumber(event) {
+  // если нажат CapsLock
+  if (event.code === "CapsLock") {
+    changeTextSize();
+  }
+
   buttons.forEach((elem) => {
+    //находим елемент, на который нажали
     if (elem.classList.contains(event.code)) {
       elem.style.backgroundColor = "#7250c0";
       elem.style.transition = "all 0.2s";
       elem.style.borderRadius = "50%";
+      //вывести в екран значение при клике на клавиатуре
+      for (children of elem.children) {
+        if (!children.classList.contains("hidden")) {
+          console.log(children);
+          keyboardScreen.innerHTML =
+            keyboardScreen.innerHTML + children.innerHTML;
+        }
+      }
     }
   });
 }
 
-function removeKeyboardStyle(event) {
+function removeKeyboardStyle() {
   buttons.forEach((elem) => {
-    if (elem.classList.contains(event.code)) {
-      elem.style.backgroundColor = "#545454";
-      elem.style.borderRadius = "0%";
-    }
+    elem.removeAttribute("style");
   });
 }
+
 document.addEventListener("keydown", showKeyboardNumber);
 document.addEventListener("keyup", removeKeyboardStyle);
