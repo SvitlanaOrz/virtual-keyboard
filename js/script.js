267de5b761675b6c1b5ee2faa52b8b46e2d74681
SvitlanaOrz <svitlana.orz@gmail.com>
2023-05-01 15:59:54 +0300

    feat: make Tab, Enter, Del buttons working

--- js/script.js
+++ js/script.js
@@ -3,147 +3,43 @@ let spetialButtons = document.querySelectorAll(".special-button");
 let textButtons = document.querySelectorAll(".text-button");
 let keyboardScreen = document.querySelector("#keyboard-screen");
 let capsLock = document.querySelector(".CapsLock");
-let pressed = new Set();
-let changeSizeCombination = ["ControlLeft", "AltLeft"];
+let shift = document.querySelectorAll(".shift");
+let tab = document.querySelectorAll(".tab");
 
 let ruSmall = document.querySelectorAll(".ru-small");
 let ruBig = document.querySelectorAll(".ru-big");
 let enSmall = document.querySelectorAll(".en-small");
 let enBig = document.querySelectorAll(".en-big");
+let enshift = document.querySelectorAll(".shift-en");
+let rushift = document.querySelectorAll(".shift-ru");
 
-function showNumber(event) {
-  keyboardScreen.innerHTML =
-    keyboardScreen.innerHTML + event.srcElement.innerText;
-}
+let unShift = document.querySelectorAll(".unshift");
+let unshiftEn = document.querySelectorAll(".unshift-en");
+let unshiftRu = document.querySelectorAll(".unshift-ru");
 
-function changeTextSize() {
-  if (ruBig[0].classList.contains("hidden")) {
-    ruBig.forEach((elem) => {
-      elem.classList.remove("hidden");
-    });
+let language = "ru";
+let wordsSize = "small";
+let pressed = new Set();
 
-    ruSmall.forEach((elem) => {
-      elem.classList.add("hidden");
-    });
+function showContent(event) {
+  if (event.srcElement.innerText === "Tab") {
+    keyboardScreen.innerHTML = keyboardScreen.innerHTML + "    ";
+  } else if (event.srcElement.innerText === "Del") {
+  } else if (event.srcElement.innerText === "Backspace") {
+    keyboardScreen.innerHTML = keyboardScreen.innerHTML.slice(
+      0,
+      keyboardScreen.innerHTML.length - 1
+    );
+  } else if (event.srcElement.innerText === "") {
+    keyboardScreen.innerHTML = keyboardScreen.innerHTML + " ";
   } else {
-    ruSmall.forEach((elem) => {
-      elem.classList.remove("hidden");
-    });
-
-    ruBig.forEach((elem) => {
-      elem.classList.add("hidden");
-    });
+    keyboardScreen.innerHTML =
+      keyboardScreen.innerHTML + event.srcElement.innerText;
   }
 }
-
-function changeLanguageOnEn() {
-  if (ruBig[0].classList.contains("hidden")) {
-    enSmall.forEach((elem) => {
-      elem.classList.remove("hidden");
-    });
-
-    ruSmall.forEach((elem) => {
-      elem.classList.add("hidden");
-    });
-  } else if (ruSmall[0].classList.contains("hidden")) {
-    enBig.forEach((elem) => {
-      elem.classList.remove("hidden");
-    });
-
-    ruBig.forEach((elem) => {
-      elem.classList.add("hidden");
-    });
-  }
-}
-
-function changeLanguageOnRu() {
-  if (enBig[0].classList.contains("hidden")) {
-    ruSmall.forEach((elem) => {
-      elem.classList.remove("hidden");
-    });
-
-    enSmall.forEach((elem) => {
-      elem.classList.add("hidden");
-    });
-  } else if (enSmall[0].classList.contains("hidden")) {
-    ruBig.forEach((elem) => {
-      elem.classList.remove("hidden");
-    });
-
-    enBig.forEach((elem) => {
-      elem.classList.add("hidden");
-    });
-  }
-}
-
-function changeLanguage() {
-  if (
-    enBig[0].classList.contains("hidden") &&
-    enSmall[0].classList.contains("hidden")
-  ) {
-    changeLanguageOnEn();
-  } else {
-    changeLanguageOnRu();
-  }
-
-  //   if (
-  //     //с русского маленький шрифт на английский
-  //     enBig[0].classList.contains("hidden") &&
-  //     enSmall[0].classList.contains("hidden") &&
-  //     ruBig[0].classList.contains("hidden")
-  //   ) {
-  //     enSmall.forEach((elem) => {
-  //       elem.classList.remove("hidden");
-  //     });
-
-  //     ruSmall.forEach((elem) => {
-  //       elem.classList.add("hidden");
-  //     });
-  //   } else if (
-  //     //с русского большой шрифт на английский
-  //     enBig[0].classList.contains("hidden") &&
-  //     enSmall[0].classList.contains("hidden") &&
-  //     ruSmall[0].classList.contains("hidden")
-  //   ) {
-  //     enBig.forEach((elem) => {
-  //       elem.classList.remove("hidden");
-  //     });
-
-  //     ruBig.forEach((elem) => {
-  //       elem.classList.add("hidden");
-  //     });
-  //   } else if (
-  //     //с английского маленький шрифт на русский
-  //     ruBig[0].classList.contains("hidden") &&
-  //     ruSmall[0].classList.contains("hidden") &&
-  //     enBig[0].classList.contains("hidden")
-  //   ) {
-  //     ruSmall.forEach((elem) => {
-  //       elem.classList.remove("hidden");
-  //     });
-
-  //     enSmall.forEach((elem) => {
-  //       elem.classList.add("hidden");
-  //     });
-  //   } else {
-  //     //с английского большой шрифт на русский
-  //     ruBig.forEach((elem) => {
-  //       elem.classList.remove("hidden");
-  //     });
-
-  //     enBig.forEach((elem) => {
-  //       elem.classList.add("hidden");
-  //     });
-  //   }
-}
-
-function openShiftContent() {}
-
-function closeShiftContent() {}
 
 function showKeyboarContent(event) {
   pressed.add(event.code);
-  console.log(event.code);
   // если нажат CapsLock
   if (event.code === "CapsLock") {
     changeTextSize();
@@ -177,18 +73,175 @@ function showKeyboarContent(event) {
   });
 }
 
+function changeTextSize() {
+  if (wordsSize === "small") {
+    if (language === "ru") {
+      ruBig.forEach((elem) => {
+        elem.classList.remove("hidden");
+      });
+      ruSmall.forEach((elem) => {
+        elem.classList.add("hidden");
+      });
+    } else if (language === "en") {
+      enBig.forEach((elem) => {
+        elem.classList.remove("hidden");
+      });
+      enSmall.forEach((elem) => {
+        elem.classList.add("hidden");
+      });
+    }
+
+    wordsSize = "big";
+  } else if (wordsSize === "big") {
+    if (language === "ru") {
+      ruSmall.forEach((elem) => {
+        elem.classList.remove("hidden");
+      });
+
+      ruBig.forEach((elem) => {
+        elem.classList.add("hidden");
+      });
+    } else if (language === "en") {
+      enSmall.forEach((elem) => {
+        elem.classList.remove("hidden");
+      });
+
+      enBig.forEach((elem) => {
+        elem.classList.add("hidden");
+      });
+    }
+
+    wordsSize = "small";
+  }
+}
+
+function changeLanguageOnEn() {
+  if (wordsSize === "small") {
+    enSmall.forEach((elem) => {
+      elem.classList.remove("hidden");
+    });
+    ruSmall.forEach((elem) => {
+      elem.classList.add("hidden");
+    });
+  } else if (wordsSize === "big") {
+    enBig.forEach((elem) => {
+      elem.classList.remove("hidden");
+    });
+    ruBig.forEach((elem) => {
+      elem.classList.add("hidden");
+    });
+  }
+  console.log(language);
+  language = "en";
+}
+
+function changeLanguageOnRu() {
+  if (wordsSize === "small") {
+    ruSmall.forEach((elem) => {
+      elem.classList.remove("hidden");
+    });
+
+    enSmall.forEach((elem) => {
+      elem.classList.add("hidden");
+    });
+  } else if (wordsSize === "big") {
+    ruBig.forEach((elem) => {
+      elem.classList.remove("hidden");
+    });
+
+    enBig.forEach((elem) => {
+      elem.classList.add("hidden");
+    });
+  }
+  console.log(language);
+  language = "ru";
+}
+
+function changeLanguage() {
+  if (language === "ru") {
+    changeLanguageOnEn();
+  } else {
+    changeLanguageOnRu();
+  }
+}
+
+function openShiftContent() {
+  changeTextSize();
+  if (language === "ru") {
+    rushift.forEach((elem) => {
+      elem.classList.remove("hidden");
+    });
+    unShift.forEach((elem) => {
+      elem.classList.add("hidden");
+    });
+    unshiftRu.forEach((elem) => {
+      elem.classList.add("hidden");
+    });
+  } else if (language === "en") {
+    enshift.forEach((elem) => {
+      elem.classList.remove("hidden");
+    });
+    unShift.forEach((elem) => {
+      elem.classList.add("hidden");
+    });
+    unshiftEn.forEach((elem) => {
+      elem.classList.add("hidden");
+    });
+  }
+}
+
+function closeShiftContent() {
+  rushift.forEach((elem) => {
+    elem.classList.add("hidden");
+  });
+  enshift.forEach((elem) => {
+    elem.classList.add("hidden");
+  });
+  unShift.forEach((elem) => {
+    elem.classList.remove("hidden");
+  });
+
+  changeTextSize();
+}
+
 function removeKeyboardStyle(event) {
   pressed.delete(event.code);
+
   let btn = document.querySelector(`.${event.code}`);
   btn.removeAttribute("style");
-  closeShiftContent();
+  if (event.code === "ShiftRight" || event.code === "ShiftLeft") {
+    closeShiftContent();
+  }
 }
 
 textButtons.forEach((elem) => {
-  elem.addEventListener("click", showNumber);
+  elem.addEventListener("click", showContent);
+});
+
+shift.forEach((elem) => {
+  elem.addEventListener("mousedown", openShiftContent);
+});
+
+shift.forEach((elem) => {
+  elem.addEventListener("mouseup", closeShiftContent);
 });
 
 capsLock.addEventListener("click", changeTextSize);
 
 document.addEventListener("keydown", showKeyboarContent);
 document.addEventListener("keyup", removeKeyboardStyle);
+
+function setLocalStorage() {
+  localStorage.setItem("language", language);
+}
+
+function getLocalStorage() {
+  console.log(language);
+  if (localStorage.getItem("language")) {
+    language = localStorage.getItem("language");
+    changeLanguage();
+  }
+}
+
+window.addEventListener("beforeunload", setLocalStorage);
+window.addEventListener("load", getLocalStorage);
